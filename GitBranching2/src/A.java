74290805580f3f8111f9a80422423c57288a912b
prafu <prafu@DESKTOP-2QAK63P>
2023-05-16 12:31:32 +0530

    By Prafull

---
+++ GitBranching2/src/A.java
@@ -0,0 +1,27 @@
+
+public class A {
+	int a = 20;
+	int b = 10;
+
+	public static void main(String[] args) 
+	{
+		System.out.println("main start");
+		
+		A a = new A();
+		a.mul();
+		a.add();
+	}
+
+	public void add()
+	{
+		int c = a+b;
+		System.out.println("addition is"+c);
+	}
+	
+	public void mul()
+	{
+		int c = a*b;
+		System.out.println("mul is"+c);
+	}
+	
+}
