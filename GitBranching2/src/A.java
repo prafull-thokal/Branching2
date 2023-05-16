d8fc99052754041c82c22913ebc452c6f11b65da
prafull-thokal <133577185+prafull-thokal@users.noreply.github.com>
2023-05-16 12:36:57 +0530

    By teamlead
    new branch

--- GitBranching2/src/A.java
+++ GitBranching2/src/A.java
@@ -6,7 +6,7 @@ public class A {
 	public static void main(String[] args) 
 	{
 		System.out.println("main start");
-		System.out.println(" branch tesla");
+		System.out.println(" branch newton");
 		
 		A a = new A();
 		a.mul();
